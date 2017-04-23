4745d352af1b734cd03dec982b234cbb78942374
arunpatro <arun.kumar.patro@gmail.com>
2017-04-23 18:13:50 +0530

    first commit

---
+++ astar.py
@@ -0,0 +1,135 @@
+#!/bin/python2.6
+
+import sys
+from math import sqrt
+
+class Tile:
+    def __init__(self, val, x, y):
+        self.val = val
+        self.g = 0
+        self.h = 0
+        self.x = x
+        self.y = y
+        self.parent = None
+        self.setReachable()
+
+    def setReachable(self):
+        self.reachable = True
+        if self.val == 1:
+            self.reachable = False
+
+    def move_cost(self, other):
+        if other.reachable:
+            return 10
+        else:
+            return 1000
+
+class AStar:
+
+    def __init__(self, maze):
+        self.maze = maze
+
+    def getAdjacent(self, cell):
+        mazeWidth = len(self.maze[0])
+        mazeHeight = len(self.maze)
+        ret = []
+        if cell.y > 0:
+            ret.append(self.getTileAtCoords(cell.x, cell.y - 1))
+        if cell.x < mazeWidth - 1:
+            ret.append(self.getTileAtCoords(cell.x + 1, cell.y))
+        if cell.y < mazeHeight - 1:
+            ret.append(self.getTileAtCoords(cell.x, cell.y + 1))
+        if cell.x > 0:
+            ret.append(self.getTileAtCoords(cell.x - 1, cell.y))
+        return ret
+
+    def heuristic(self, cell):
+        return sqrt((self.end.x - cell.x)**2 + (self.end.y - cell.y)**2)
+
+    def constructPath(self, current):
+        path = []
+        while current.parent:
+            path.append(current)
+            current = current.parent
+        path.append(current)
+        return path[::-1]
+
+    def getTileAtCoords(self, x, y):
+        try:
+            return self.maze[y][x]
+        except KeyError as e:
+            print "Could not find tile at position x: {0} y: {1}".format(x, y)
+
+    def search(self, current, end):
+        self.end = end
+        openset = set()
+        closedset = set()
+        openset.add(current)
+        while len(openset):
+            current = min(openset, key=lambda o:o.g + o.h)
+            if current == end:
+                return self.constructPath(current)
+            openset.remove(current)
+            closedset.add(current)
+            for node in self.getAdjacent(current):
+                if node in closedset:
+                    continue
+                if node in openset:
+                    new_g = current.g + current.move_cost(node)
+                    if node.g > new_g:
+                        node.g = new_g
+                        node.parent = current
+                else:
+                    node.g = current.g + current.move_cost(node)
+                    node.h = self.heuristic(node)
+                    node.parent = current
+                    openset.add(node)
+
+def getMaze():
+    maze = []
+    for i ,line in enumerate(sys.stdin):
+        line = list (line.rstrip("\r\n"))
+        row = []
+        for j in range(len(line)):
+            row.append(Tile(line[j], j, i))
+        maze.append(row)
+    return maze
+
+def fillPath(maze):
+    aStar = AStar(maze)
+    for tile in aStar.search(maze[0][0], maze[-1][-1]):
+        maze[tile.y][tile.x].val = 3
+
+def printMaze(maze):
+    for row in maze:
+        for tile in row:
+            sys.stdout.write(tile.val)
+        print
+
+def getMaze2(grid):
+    maze = []
+    for i ,line in enumerate(grid):
+        row = []
+        for j in range(len(line)):
+            row.append(Tile(line[j], j, i))
+        maze.append(row)
+    return maze
+
+
+
+if __name__ == "__main__":
+
+    grid = [[0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0, 0],
+        [0, 1, 1, 0, 0, 1],
+        [0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 2]]
+
+    # maze = getMaze()
+    maze2 = getMaze2(grid)
+    # print(len(maze))
+    # print(maze)
+    fillPath(maze2)
+    for line in maze2:
+        print [i.val for i in line]
---
+++ harsh.py
@@ -0,0 +1,20 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv2.imread('harsh.jpg');
+rows,cols,ch = img.shape
+pts1 = np.float32([[0,150],[1725,178],[39,1303],[1695,1240]])
+pts2 = np.float32([[0,0],[1500,0],[0,1000],[1500,1000]])
+
+M = cv2.getPerspectiveTransform(pts1,pts2)
+dst = cv2.warpPerspective(img,M,(1500,1000))
+
+gray = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
+thres = cv2.threshold(gray,150,255,cv2.THRESH_BINARY)[1]
+
+kernel = np.ones((5,5),np.uint8)
+closing = cv2.morphologyEx(thres, cv2.MORPH_CLOSE, kernel,iterations = 10)
+
+plt.imshow(closing,cmap='gray')
+plt.show()
